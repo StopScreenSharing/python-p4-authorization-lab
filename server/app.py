63736befa65282b8dc7a9ce87619ea2fd77ddc61
aidadieudonne <aidaharing10@gmail.com>
2025-11-10 17:51:09 -0700

    add authorization to blog for members only

--- server/app.py
+++ server/app.py
@@ -87,12 +87,26 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        if not session.get('user_id'):
+            return {'error': 'Unauthorized'}, 401
+        
+        
+        articles = Article.query.filter_by(is_member_only=True).all()
+        articles_json = [article.to_dict() for article in articles]
+        return make_response(jsonify(articles_json), 200)
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+      user_id = session.get('user_id')
+      if not user_id:
+          return {}, 401
+      
+      article = Article.query.get(id)
+      if not Article:
+          return {'error': 'Article not found'}, 404
+      
+      return article.to_dict(), 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
